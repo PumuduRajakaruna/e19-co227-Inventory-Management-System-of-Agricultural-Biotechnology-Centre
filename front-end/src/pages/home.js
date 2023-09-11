2cb877f94253b3070235ec3dc4d5fe1ab98e9617
Pumudu Nadun Rajakaruna <e19306@eng.pdn.ac.lk>
2023-09-11 13:05:30 +0530

    add home page

---
+++ front-end/src/pages/home.js
@@ -0,0 +1,9 @@
+import REACT from "react";
+
+export default function Home() {
+    return (
+      <div className='body'>
+        <h1>Inventory Management System Of Agricultural Biotechnology Centre</h1>
+      </div>
+    )
+  }
